54a396c571fb5b43888db8681cccb3f91a8832b7
KIDPhantom <xiangkaicheng@foxmail.com>
2026-03-13 20:39:59 +0800

    重启工作进程时，继续开启异步日志

--- MyRPC/core/service.cpp
+++ MyRPC/core/service.cpp
@@ -102,6 +102,7 @@ pid_t MyRPCService::restartWorker(pid_t oldPid) {
   }
   if (0 == pid) {
     is_master_ = false;
+    LOGGER.EnableAsync();
     reactor_.Run(&config_);  // 子进程启动reactor，陷入事件监听，不会再返回
   }
   // 只有父进程会执行到这里
